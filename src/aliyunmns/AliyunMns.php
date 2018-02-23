5569604795704888d384deefcaedabc5d00c787c
git <git@222.84.159.240>
2018-02-23 18:39:51 +0800

    批量发送和发送查询

--- src/aliyunmns/AliyunMns.php
+++ src/aliyunmns/AliyunMns.php
@@ -2,6 +2,8 @@
 
 namespace Buerxiaojie\AliyunMns;
 
+use Aliyun\Api\Sms\Request\V20170525\QuerySendDetailsRequest;
+use Aliyun\Api\Sms\Request\V20170525\SendBatchSmsRequest;
 use Aliyun\Api\Sms\Request\V20170525\SendSmsRequest;
 use Aliyun\Core\Config as AliyunConfig;
 use Aliyun\Core\DefaultAcsClient;
@@ -23,13 +25,12 @@ class AliyunMns implements MNS
         $this->endPoint  = config('aliyunmns.MnsEndpoint'); // eg. http://1234567890123456.mns.cn-shenzhen.aliyuncs.com
         $this->accessId  = config('aliyunmns.accessID');
         $this->accessKey = config('aliyunmns.accessKey');
-        $this->client    = new Client($this->endPoint, $this->accessId, $this->accessKey);
 
         AliyunConfig::load();
 
     }
 
-    public static function getAcsClient()
+    public function getAcsClient()
     {
         //产品名称:云通信流量服务API产品,开发者无需替换
         $product = "Dysmsapi";
@@ -89,6 +90,71 @@ class AliyunMns implements MNS
         // 选填，上行短信扩展码（扩展码字段控制在7位或以下，无特殊需求用户请忽略此字段）
         if ($upExtendCode) {
             $request->setSmsUpExtendCode($upExtendCode);
+        }
+
+        // 发起访问请求
+        $acsResponse = static::getAcsClient()->getAcsResponse($request);
+
+        return $acsResponse;
+    }
+
+    /**
+     * 批量发送短信
+     * @return stdClass
+     */
+    public static function sendBatch($phoneNumbers, $signNames, $templateCode, array $templateParam = [])
+    {
+
+        // 初始化SendSmsRequest实例用于设置发送短信的参数
+        $request = new SendBatchSmsRequest();
+
+        // 必填:待发送手机号。支持JSON格式的批量调用，批量上限为100个手机号码,批量调用相对于单条调用及时性稍有延迟,验证码类型的短信推荐使用单条调用的方式
+        $request->setPhoneNumberJson(json_encode($phoneNumbers, JSON_UNESCAPED_UNICODE));
+
+        // 必填:短信签名-支持不同的号码发送不同的短信签名
+        $request->setSignNameJson(json_encode($signNames, JSON_UNESCAPED_UNICODE));
+
+        // 必填:短信模板-可在短信控制台中找到
+        $request->setTemplateCode($templateCode);
+
+        // 必填:模板中的变量替换JSON串,如模板内容为"亲爱的${name},您的验证码为${code}"时,此处的值为
+        // 友情提示:如果JSON中需要带换行符,请参照标准的JSON协议对换行符的要求,比如短信内容中包含\r\n的情况在JSON中需要表示成\\r\\n,否则会导致JSON在服务端解析失败
+        $request->setTemplateParamJson(json_encode($templateParam, JSON_UNESCAPED_UNICODE));
+
+        // 可选-上行短信扩展码(扩展码字段控制在7位或以下，无特殊需求用户请忽略此字段)
+        // $request->setSmsUpExtendCodeJson("[\"90997\",\"90998\"]");
+
+        // 发起访问请求
+        $acsResponse = static::getAcsClient()->getAcsResponse($request);
+
+        return $acsResponse;
+    }
+
+    /**
+     * 短信发送记录查询
+     * @return stdClass
+     */
+    public static function querySendDetails($phoneNumber, $sendDate, $pageSize, $currentPage, $bizId = '')
+    {
+
+        // 初始化QuerySendDetailsRequest实例用于设置短信查询的参数
+        $request = new QuerySendDetailsRequest();
+
+        // 必填，短信接收号码
+        $request->setPhoneNumber($phoneNumber);
+
+        // 必填，短信发送日期，格式Ymd，支持近30天记录查询
+        $request->setSendDate($sendDate);
+
+        // 必填，分页大小
+        $request->setPageSize($pageSize);
+
+        // 必填，当前页码
+        $request->setCurrentPage($currentPage);
+
+        // 选填，短信发送流水号
+        if ($bizId) {
+            $request->setBizId($bizId);
         }
 
         // 发起访问请求
